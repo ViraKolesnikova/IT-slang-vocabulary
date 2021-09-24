250996d0616fc9d5266d93d39924c8e33ac9eb35
ViraKolesnikova <82116461+ViraKolesnikova@users.noreply.github.com>
2021-09-24 21:44:34 +0300

    added jokes & test div

---
+++ js/apps.js
@@ -0,0 +1,133 @@
+import data from './data.js';
+import refs from './refs.js';
+// console.log(data);
+// console.log(refs);
+
+
+const { inputRef, searchBtnRef, alphabetListRef, termRef, definitionRef, listRef, searchForm, headerRef, mainRef, themeSwitcherRef, bodyRef } = refs;
+
+// EVENTS
+searchBtnRef.addEventListener('click', onInputSearch);
+inputRef.addEventListener('focus', onInputFocus);
+alphabetListRef.addEventListener('click', onLetterClick);
+listRef.addEventListener('click', onTermLinkClick);
+themeSwitcherRef.addEventListener('change', onThemeSwitcherChange);
+
+changeMainPositionToHeader();
+loadSavedTheme();
+
+// INPUT FUNCTIONS
+function onInputSearch(event) {
+  event.preventDefault();
+  listRef.innerHTML = '';
+  const result = data.find(object => {
+    return inputRef.value.toLowerCase() === object.term.toLowerCase();
+  })
+ result === undefined ? isAbsentDefinition() : getSearchedDefinition(result);
+}
+
+function onInputFocus(event) {
+  clearInput();
+  showDefaultDefinition();
+  listRef.innerHTML = '';
+}
+
+function getSearchedDefinition(obj) {
+  termRef.textContent = obj.term;
+  definitionRef.textContent = obj.meaning;
+  clearInput();
+}
+
+function isAbsentDefinition() {
+  termRef.textContent = inputRef.value;
+  definitionRef.textContent = 'Нажаль, в словнику не знайдено такого терміну...'; 
+}
+
+function showDefaultDefinition() {
+  termRef.textContent = 'Термін';
+  definitionRef.textContent = 'Тлумачення';
+}
+
+function clearInput() {
+  inputRef.value = '';
+}
+
+
+// ALPHABET FUNCTIONS
+function onLetterClick(event) {
+  clearInput();
+  listRef.replaceChildren();
+  if (event.target.nodeName !== 'BUTTON') {
+    return
+  };
+  const filteredArr = [];
+  const activeEl = event.target;
+  
+  definitionRef.textContent = '';
+  termRef.textContent = `"${event.target.textContent}"`;
+  filterTermsByLetter(filteredArr, activeEl);
+  const listItems = createSortedByAlphabetListItems(filteredArr);
+  
+  listRef.insertAdjacentHTML('afterbegin', listItems);
+}
+
+function onTermLinkClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'A') {
+    return
+  };
+  const foundObj = data.find(object => {
+    return event.target.textContent.toLowerCase() === object.term.toLowerCase();
+  })
+  listRef.innerHTML = '';
+  termRef.textContent = foundObj.term;
+  definitionRef.textContent = foundObj.meaning;
+}
+
+function filterTermsByLetter(arr, elem) {
+   data.filter(obj => {
+    return elem.textContent === obj.term[0].toLocaleUpperCase();
+   }).forEach(obj => arr.push(obj.term));
+  return arr;
+}
+
+function createSortedByAlphabetListItems(arr) {
+  const list = arr.sort((a, b) => {
+    return a.localeCompare(b)
+  }).map(elem => {
+    return `<li class = "word-list-item"><a href="" class="word-list-link">${elem}</a></li>`
+  }).join('');
+  return list;
+}
+
+
+// ====================
+function changeMainPositionToHeader() {
+  mainRef.style.paddingTop = headerRef.offsetHeight + 'px';
+  console.log(mainRef.style.marginTop);
+}
+
+// TOGGLE SWITCH
+function onThemeSwitcherChange(event) {
+  bodyRef.classList.contains('dark-theme') ? changeToLightTheme() : changeToDarkTheme()
+}
+
+function changeToDarkTheme() {
+  bodyRef.classList.add('dark-theme');
+  themeSwitcherRef.checked = true;
+  localStorage.setItem('theme', 'dark');
+}
+
+function changeToLightTheme() {
+  bodyRef.classList.remove('dark-theme');
+  themeSwitcherRef.checked = false;
+  localStorage.removeItem('theme');
+}
+
+// SAVE THEME
+function loadSavedTheme() {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    return changeToDarkTheme();
+  }
+}
---
+++ js/refs.js
@@ -0,0 +1,13 @@
+export default
+{
+  inputRef: document.querySelector('.search-input'),
+  searchBtnRef: document.querySelector('.search-button'),
+  alphabetListRef: document.querySelector('.alphabet-list'),
+  termRef: document.querySelector('.searched-term'),
+  definitionRef: document.querySelector('.word-definition'),
+  listRef: document.querySelector('.word-list'),
+  headerRef: document.querySelector('header'),
+    mainRef: document.querySelector('main'),
+  bodyRef: document.querySelector('body'),
+  themeSwitcherRef: document.querySelector('.switch_3')
+}
